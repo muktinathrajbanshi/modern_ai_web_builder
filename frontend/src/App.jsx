35ef62fded74fae26141cd9a14cd189939ed2625
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-24 23:20:05 +0545

    protected routes logic builded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./pages/Layout";
+import { AuthLayout, GuestLayout } from "./pages/Layout";
 import AuthPage from "./pages/AuthPage";
+import BuilderPage from "./pages/BuilderPage";
+import PreviewPage from "./pages/PreviewPage";
 
 const App = () => {
   return (
     <Routes>
       {/* Login Routes  */}
-      <Route element={<Layout />}>
+      <Route element={<GuestLayout />}>
         <Route path="/login" element={<AuthPage mode="login" />} />
         <Route path="/register" element={<AuthPage mode="register" />} />
+      </Route>
+
+      {/* Protected Routes  */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/builder/:id" element={<BuilderPage />} />
+        <Route path="/preview/:id" element={<PreviewPage />} />
       </Route>
     </Routes>
   );
