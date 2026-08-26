ed03541a07dbb197b19f8104326d78d5be178924
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-26 21:38:34 +0545

    Nidra lago my lord

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <div className="min-h-screen bg-white flex text-zinc-900 font-sans">
+      {/* Left Panel - Branding  */}
+
+      {/* Right Panel - Form  */}
+    </div>
+  );
 };
 
 export default AuthPage;
