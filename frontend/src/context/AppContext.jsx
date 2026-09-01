f288e3e31559fa8c940b5adc0c17c6e107c352bf
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-01 21:40:50 +0545

    wow vidu ye kiya hora hain mere sath

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -16,7 +16,7 @@ export function AppContextProvider({ children }) {
   const checkSession = async () => {
     try {
       const { data } = await api.get("/api/auth/me");
-      // setUser(data.user);
+      setUser(data.user);
     } catch (error) {
       setUser(null);
     } finally {
