a4845eeb256e404cc8636f09e094c113ce6b1168
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-03 22:05:46 +0545

    wah vidu kiya baat hain

--- frontend/src/components/PromptInput.jsx
+++ frontend/src/components/PromptInput.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CloudUploadIcon } from "lucide-react";
 
 const PromptInput = ({
   onSubmit,
@@ -45,6 +46,17 @@ const PromptInput = ({
           rows={3}
           className="w-full p-4 pb-2 resize-none placeholder:text-white/60 outline-none bg-transparent text-white text-base"
         />
+
+        <div className="flex items-center justify-between pb-3 px-3 gap-2">
+          <label
+            htmlFor="file"
+            className="border border-white/20 text-white/80 hover:text-white
+          hover:border-white/30 p-1.5 rounded-md cursor-pointer flex items-center justify-center"
+          >
+            <input type="file" id="file" hidden />
+            <CloudUploadIcon size={18} />
+          </label>
+        </div>
       </form>
     );
   }
