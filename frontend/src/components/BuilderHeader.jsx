621bd3d47a1e5f4bac1220c2a9baf5abd3aaaacb
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-11 22:58:30 +0545

    oi hoki kiya ho

--- frontend/src/components/BuilderHeader.jsx
+++ frontend/src/components/BuilderHeader.jsx
@@ -1,3 +1,5 @@
+import { ArrowLeftIcon } from "lucide-react";
+
 const BuilderHeader = ({
   projectName,
   version,
@@ -11,7 +13,18 @@ const BuilderHeader = ({
   onLogout,
 }) => {
   return (
-    <header className="h-12 shrink-0 flex items-center justify-between px-3 border-b border-zinc-200 bg-white"></header>
+    <header className="h-12 shrink-0 flex items-center justify-between px-3 border-b border-zinc-200 bg-white">
+      <div className="flex items-center gap-2">
+        <button
+          onClick={onBack}
+          className="p-1.5 rounded-md text-zinc-400 hover:text-zinc-950 hover:bg-zinc-100 cursor-pointer"
+        >
+          <ArrowLeftIcon size={16} />
+        </button>
+        <img src="/logo.svg" alt="img" className="invert size-5" />
+        <span>{projectName}</span>
+      </div>
+    </header>
   );
 };
 
