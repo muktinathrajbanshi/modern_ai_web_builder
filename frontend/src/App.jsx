a12b68b083bbe37699df655812026aa7bdcac9ff
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-25 22:53:43 +0545

    created project builder page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,32 @@ import BuilderPage from "./pages/BuilderPage";
 import PreviewPage from "./pages/PreviewPage";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import PublishPage from "./pages/PublishPage";
 
 const App = () => {
   return (
     <>
-    <Toaster />
-    <Routes>
-      {/* Login Routes  */}
-      <Route element={<GuestLayout />}>
-        <Route path="/login" element={<AuthPage mode="login" />} />
-        <Route path="/register" element={<AuthPage mode="register" />} />
-      </Route>
+      <Toaster />
+      <Routes>
+        {/* Login Routes  */}
+        <Route element={<GuestLayout />}>
+          <Route path="/login" element={<AuthPage mode="login" />} />
+          <Route path="/register" element={<AuthPage mode="register" />} />
+        </Route>
 
-      {/* Protected Routes  */}
-      <Route element={<AuthLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/builder/:id" element={<BuilderPage />} />
-        <Route path="/preview/:id" element={<PreviewPage />} />
-      </Route>
+        {/* Protected Routes  */}
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/builder/:id" element={<BuilderPage />} />
+          <Route path="/preview/:id" element={<PreviewPage />} />
+        </Route>
 
-      {/* Catch-all */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        {/* Public Routes  */}
+        <Route path="/publish/:id" element={<PublishPage />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 };
